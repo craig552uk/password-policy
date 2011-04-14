cb17bd09e87cda522cb168b449ecb55d72385700
Craig Russell <craig@craig-russell.co.uk>
2011-04-14 14:58:58 +0100

    Form displays helpful information from class

--- policy-demo.php
+++ policy-demo.php
@@ -4,8 +4,23 @@
     require_once('password-policy.php');
     
     // Get password from submited data or default
-    $password = ( isset($_POST['password']) ) ? $_POST['password'] : '';
-
+    $password = ( isset($_POST['password']) ) ? $_POST['password'] : false;
+    
+    // Array defining rules
+    $rules['min_numeric_chars'] = 3;
+    $rules['disallow_numeric_chars'] = true;
+    
+    // Create password policy object
+    // Pass rule array in constructor
+    $policy = new PasswordPolicy($rules);
+    
+    // Rules defined against object
+    $policy->min_length = 8;
+    $policy->max_length = 12;
+    
+    // Validate submitted password
+    $valid = $policy->validate($password);
+    
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,19 +30,34 @@
     <style>
         body {font-family: sans-serif;}
         .error {color: red;}
+        .valid {color: green; font-weight: bold;}
     </style>
 </head>
 <body>
     <form name="form" method="post">
         <h1>Validate Password</h1>
-        <p class="error">Error Blagh</p>
+        <?php
+            // Show errors if submitted password is not valid
+            if( !$valid && isset($_POST['submit']) )
+            foreach( $policy->get_errors() as $k=>$error )
+            echo "<p class=\"error\" id=\"$k\">$error</p>";
+        ?>
         <p>
             <label for="password">Password</label>
             <input type="text" name="password" id="password" value="<?php echo $password; ?>"/>
+            <?php if( $valid ) echo "<span class=\"valid\">Password OK!</span>"; ?>
         </p>
         <p>
             <input type="submit" name="submit" value="Validate"/>
         </p>
     </form>
+    <section>
+        <h2>Password Policy Rules</h2>
+        <?php
+            // Show defined Password Policy Rules
+            foreach( $policy->policy() as $k=>$rule )
+            echo "<p id=\"$k\"><em>$rule</em></p>";
+        ?>
+    </section>
 </body>
 </html>
